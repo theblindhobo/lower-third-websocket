0335d89764462a684b92c3fa385b88d3f4869571
theblindhobo <theblindhobo@gmail.com>
2022-01-30 09:26:53 -0700

    Added feature to write default Demoscene title to Scheduler's title log file

--- functions/useData.js
+++ functions/useData.js
@@ -1,4 +1,22 @@
+var fs = require('fs');
 const { formatMetadata } = require('./formatMetadata.js');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const title1 = process.env.TITLE1;
+
+function nowDemoscene() {
+  const d = new Date(Date.now()).toLocaleDateString('en-US',
+    {
+      timeZone: 'Asia/Tokyo',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    }
+  ).replace(',','').split(' ');
+  let dT = `NOW: Demoscene | ${d[1]} ${d[0]} ${d[2]}`;
+  return dT;
+}
 
 module.exports = {
   useData: async (data, ws) => {
@@ -6,6 +24,12 @@ module.exports = {
       if(data != 'NOT_PLAYING') {
         let metadata = await formatMetadata(data); // formats to JSON
         console.log(`\x1b[36m%s\x1b[32m%s\x1b[0m`, `[LOWER THIRD]`, ` Now playing:`, metadata.filename);
+        try{
+          // write NOW: Demoscene to title1.txt file in Scheduler app
+          fs.writeFileSync(title1, nowDemoscene());
+        } catch(err) {
+          console.log(`\x1b[33m%s\x1b[0m`, `[WRITEFILE]`, `Could not write to title1.txt file.`);
+        }
         let vlcObj = {
           "event": "message",
           "action": "VLC",
